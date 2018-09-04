71c325ec88cf1f4e182d84ea7b5f94df1f6d2069
rakshitgupta22 <42322981+rakshitgupta22@users.noreply.github.com>
2018-09-04 23:20:26 +0530

    new

--- admin_category/assets/js/Vertical-menu.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-    $("#vertical-menu .list-group-item").click(function () {
-        $("#vertical-menu .list-group-item").removeClass("active");
-        $(this).addClass("active");
-    });
-});
